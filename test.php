5d4c7b5891469b4cea2d7ac87d14aa0f8ca85a07
A-Krivoshein <a.krivoshein@gmail.com@>
2017-10-09 12:27:08 +0300

    if isset remove to functon2

--- test.php
+++ test.php
@@ -48,7 +48,7 @@ $test = json_decode($test, true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Тест:</title>
 </head>
 <body>
 
